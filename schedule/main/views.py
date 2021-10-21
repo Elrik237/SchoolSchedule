a9626e6b0c448452ae07134998fdbc72a9167b16
SokolovichIlya <git config --global user.email>
2021-10-21 20:53:07 +0300

    init

---
+++ schedule/main/views.py
@@ -0,0 +1,44 @@
+from django.http.response import JsonResponse
+from django.shortcuts import render
+from django.views.generic.base import TemplateView, View
+
+class HomePageView(TemplateView):
+
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = {'test': 'test'}
+        return context
+
+class SearchSchedule(View):
+    def get(self, request):
+        select_value = int(request.GET.get('selectValue'))
+        select_date = request.GET.get('selectDate')
+
+        data1 = [
+            {'time': '08:00 - 08:40', 'discipline': 'Родной язык', 'name': 'Иванов И.И.', 'place': 'Каб. 11'},
+            {'time': '08:00 - 08:40', 'discipline': 'Родной язык', 'name': 'Иванов И.И.', 'place': 'Каб. 11'},
+            {'time': '08:00 - 08:40', 'discipline': 'Родной язык', 'name': 'Иванов И.И.', 'place': 'Каб. 11'},
+            {'time': '08:00 - 08:40', 'discipline': 'Родной язык', 'name': 'Иванов И.И.', 'place': 'Каб. 11'},
+            {'time': '08:00 - 08:40', 'discipline': 'Родной язык', 'name': 'Иванов И.И.', 'place': 'Каб. 11'},
+            {'time': '08:00 - 08:40', 'discipline': 'Родной язык', 'name': 'Иванов И.И.', 'place': 'Каб. 11'},
+            {'time': '08:00 - 08:40', 'discipline': 'Родной язык', 'name': 'Иванов И.И.', 'place': 'Каб. 11'},
+            {'time': '08:00 - 08:40', 'discipline': 'Родной язык', 'name': 'Иванов И.И.', 'place': 'Каб. 11'},
+        ] 
+
+        data2 = [
+            {'time': '08:00 - 08:40', 'discipline': 'Родной язык', 'name': 'Петров П.П.', 'place': 'Каб. 11'},
+            {'time': '08:00 - 08:40', 'discipline': 'Родной язык', 'name': 'Петров П.П.', 'place': 'Каб. 11'},
+            {'time': '08:00 - 08:40', 'discipline': 'Родной язык', 'name': 'Петров П.П.', 'place': 'Каб. 11'},
+            {'time': '08:00 - 08:40', 'discipline': 'Родной язык', 'name': 'Петров П.П.', 'place': 'Каб. 11'},
+            {'time': '08:00 - 08:40', 'discipline': 'Родной язык', 'name': 'Петров П.П.', 'place': 'Каб. 11'},
+        ]
+
+        data = None
+
+        if select_value == 1:
+            data = data1
+        else:
+            data = data2
+
+        return JsonResponse(data = data, safe=False)
