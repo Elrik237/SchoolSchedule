2e70361af0bad7a2b5834558ebd92d880e47f632
Elrik237 <Elrik237@gmail.com>
2021-10-25 18:14:32 +0300

    Создание моделей бд

--- schedule/main/admin.py
+++ schedule/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Teachers, Groups, Schedule
 
-# Register your models here.
+admin.site.register(Teachers)
+admin.site.register(Groups)
+admin.site.register(Schedule)
+
--- schedule/main/models.py
+++ schedule/main/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Teachers (models.Model):
+
+    fio = models.CharField("ФИО преподавателя" ,max_length=150)
+
+    class Meta:
+        verbose_name = 'Список преподавателей'
+        verbose_name_plural = 'Список преподавателей'
+
+    def __str__(self):
+        return self.fio
+
+class Groups (models.Model):
+
+    name = models.CharField("Класса" ,max_length=3)
+
+    class Meta:
+        verbose_name = 'Список классов'
+        verbose_name_plural = 'Список классов'
+
+    def __str__(self):
+        return self.name
+
+class Schedule (models.Model):
+
+    day = models.DateField("Дата" ,max_length=150)
+    time = models.CharField("Время" ,max_length=150)
+    discipline = models.CharField("Дисциплина" ,max_length=250)
+    teacher  = models.CharField("ФИО преподавателя" ,max_length=150)
+    group  = models.ForeignKey(Groups, on_delete=models.CASCADE,)
+    place = models.ForeignKey(Teachers, on_delete=models.CASCADE,)
+
+    class Meta:
+        verbose_name = 'Рассписание уроков'
+        verbose_name_plural = 'Рассписание уроков'
+
+    def __str__(self):
+        return "Schedule"
